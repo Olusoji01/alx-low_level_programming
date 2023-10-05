682e2f85b57d65efc9a1aed446ee337f8189aaa9
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-05 03:02:47 -0700

    a C program that prints exactly string with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - A program that print a string using
+ * printf function
+ * void: no argument is passed to the function
+ * Return: the program return 0 after execution
+ */
+
+int main(void)
+{
+	char str[57] = "with proper grammar, but the outcome is a piece of art,";
+
+	/*Printing the string with printf function*/
+	printf("%s\n", str);
+
+	return (0);
+}
