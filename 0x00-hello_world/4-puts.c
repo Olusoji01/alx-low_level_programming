000fcb295f66996e41c11f3bd50b41717f013132
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-05 02:45:18 -0700

    a C program that print a string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main- a program that print string using puts
+ * Return: the program return 0 after execution
+ * void: no argument is passed to the main function
+ */
+
+int main(void)
+{
+	char str[52] = "Programming is like building a multilingual puzzle";
+
+/* using puts to print the string char array*/
+	puts(str);
+
+	return (0);
+}
