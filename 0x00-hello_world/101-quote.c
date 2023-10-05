2aefe4c2e9d451f1cce4873c332da00b8f41f7bb
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-05 05:17:35 -0700

    a C program that prints string without puts or printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 int main(void)
 {
 	/*Declaring a variable i.e message and assign the string value to it*/
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *message = "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n";
 
 	/*writing out the message using write function*/
 	write(2, message, 59);
