96aefeb8b41855dc60980e5786f4f6883492ef8d
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-06 04:25:31 -0700

    a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line
+ * void: no argument is passed
+ */
+int main(void)
+{
+	/* Declaring variables */
+	char str;
+	char str_u;
+
+	/* using for loop to print the alphabets */
+	for (str = 'a'; str <= 'z'; str++)
+	{
+		putchar(str);
+	}
+	for (str_u = 'A'; str_u <= 'Z'; str_u++)
+	{
+		putchar(str_u);
+	}
+	putchar('\n');
+
+	return (0);
+}
