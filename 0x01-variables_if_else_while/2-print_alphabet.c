e967656582d8c70318d7ec5107aabf89d79bdf73
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-06 04:02:56 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet 
+ * in lowercase followed by a new line
+ * void: nothing is passed as an argument
+ * Return: it return zero after execution
+ */
+int main(void)
+{
+	/* Declaring a variable and assigning a value to it */
+	char str = 'a';
+
+	/* looping through the variable */
+	for (str = 'a'; str <= 'z'; str++)
+	{
+		putchar(str);
+	}
+	putchar('\n');
+
+	return (0);
+}
