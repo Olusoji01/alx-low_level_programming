ce3684ae27803943e3e8ea54e73b97fc9e93e5f0
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-10 15:42:23 -0700

    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,12 +16,6 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				if (j == 0)
-				{
-					_putchar(j + '0');
-				}
-				else
-				{
 					if ((i * j) < 10 && j != 0)
 					{
 						_putchar(',');
@@ -46,7 +40,10 @@ void print_times_table(int n)
 						_putchar(((i * j / 10) % 10) + '0');
 						_putchar((i * j) % 10 + '0');
 					}
-				}
+					else if (j == 0)
+					{
+						_putchar(i * j + '0');
+					}
 			}
 			_putchar('\n');
 		}
