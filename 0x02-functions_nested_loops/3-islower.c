25068b253d55370bfdedcc2c62656fc89539ce5d
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-10 05:12:15 -0700

    a function that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - a function that checks for lowercase character
  * @c: character to check if its lower
- * Returns: it returns 0 at execution
+ * Return: it returns 1 if its lower and 0 if upper
  */
 int _islower(int c)
 {
