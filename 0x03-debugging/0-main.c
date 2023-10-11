8270548d466091e1e71ab86a507142d2ec4298fb
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-11 09:00:52 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - testing a program to display 0
+ * void: No value is passed as an argument
+ * Return: it return 0 after execution
+ */
+int main(void)
+{
+	        int i;
+
+	        i = 0;
+		positive_or_negative(i);
+
+		return (0);
+}
