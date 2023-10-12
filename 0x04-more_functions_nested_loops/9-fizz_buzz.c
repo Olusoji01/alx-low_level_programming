cc5eb724eb586308a6cd36e4ad079700e02afde8
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-12 08:24:43 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,23 +6,23 @@
  */
 int main(void)
 {
-	int i = 1;
+	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) != 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if ((i % 5) == 0 && (i % 3) != 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
-		else if (i == 100)
+		else if (i == 1)
 		{
 			printf("%d", i);
 		}
