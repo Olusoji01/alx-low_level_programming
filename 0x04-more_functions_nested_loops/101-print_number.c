d63cba404e2361389b78dd36723666aeadc3c3d8
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-13 04:39:00 -0700

    a function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,30 @@
  */
 void print_number(int n)
 {
+	int divisor;
+	int temp;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10 != 0)
+	divisor = 1;
+	temp = n;
+
+	while (temp / 10 > 0)
 	{
-		print_number(n / 10);
+		divisor *= 10;
+		temp /= 10;
 	}
-	_putchar(n % 10 + '0');
+	while (divisor > 0)
+	{
+		int digit = n / divisor;
+
+		_putchar(digit + '0');
+
+		n -= digit * divisor;
+
+		divisor /= 10;
+	}
 }
