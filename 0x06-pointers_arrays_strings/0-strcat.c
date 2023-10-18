c80c3e9990f212d0fd007acb490a6e97b933dcd1
Ajayi Sunday <ajayisunday64@gmail.com>
2023-10-18 05:14:54 -0700

    a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: destination
+ * @src: string to be copied
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
