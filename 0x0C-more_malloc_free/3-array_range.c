4e462f5b85673af23320e21be36cd55d0b39f7da
Ajayi Sunday <ajayisunday64@gmail.com>
2023-11-02 06:00:47 -0700

    a function that creates an array of integers.

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,15 +14,16 @@ int *array_range(int min, int max)
 	if (min > max)
 	{
 		return (NULL);
-	}
+	} 
 	ptr = (int *)malloc((max - min + 1) * sizeof(int));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < (max - min + 1); i++)
+	for (i = 0; i <= max; i++)
 	{
-		ptr[i] = min + 1;
+		ptr[i] = min;
+		min++;
 	}
 	return (ptr);
 }
