dfc84ae2ea1cec83f770dcd184580021cb5fee48
Ajayi Sunday <ajayisunday64@gmail.com>
2023-11-06 11:49:26 -0800

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - a function that creates a new dog
  * @name: name of new dog
@@ -16,9 +17,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	new_dog->name = name;
+	new_dog->name = strdup(name);
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = strdup(owner);
+
 
 	return (new_dog);
 }
