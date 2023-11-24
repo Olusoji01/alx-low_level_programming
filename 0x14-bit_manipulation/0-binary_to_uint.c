8a780dcde611843886b9c789280bdd7fbd0403e6
Ajayi Sunday <ajayisunday64@gmail.com>
2023-11-24 02:15:13 -0800

    a function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,7 @@
  * binary_to_uint - a function that converts
  * a binary number to an unsigned int
  * @b: characcter pointing to a string of 0 and 1
- * Returns: it returns the converted number at success
+ * Return: it returns the converted number at success
  * and 0 if there is one or more chars in string that
  * is not 0 and 1
  */
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	}
 	while (*b != '\0')
 	{
-		if (*b == '0'|| *b == '1')
+		if (*b == '0' || *b == '1')
 		{
 			result = (result << 1) | (*b - '0');
 		}
